e77e1f81e74d4b59b9c6b68b651b8e26c1ccdfce
Ger van Diepen <gervandiepen@gmail.com>
2008-09-16 11:29:26 +0000

    Added the possibility to do cleaning
    git-svn-id: https://svn.astron.nl/casarest/trunk/casarest@6379 4215e939-9a1b-0410-8a78-adf775e9a44f

--- synthesis/apps/lwimager.cc
+++ synthesis/apps/lwimager.cc
@@ -1,4 +1,4 @@
-//# lwimager.cc: Program to convert an image to FITS format
+//# lwimager.cc: Program to create and/or clean an image
 //# Copyright (C) 2008
 //# Associated Universities, Inc. Washington DC, USA.
 //#
@@ -30,16 +30,47 @@
 #include <synthesis/MeasurementEquations/Imager.h>
 #include <images/Images/PagedImage.h>
 #ifdef HAVE_HDF5
-#include <images/Images/HDF5Image.h>
+# include <images/Images/HDF5Image.h>
 #endif
 #include <images/Images/ImageFITSConverter.h>
 #include <casa/Inputs.h>
 #include <casa/Arrays/ArrayUtil.h>
 #include <casa/Utilities/Regex.h>
+#include <casa/Utilities/Assert.h>
 #include <casa/Exceptions/Error.h>
 #include <casa/iostream.h>
+#include <casa/sstream.h>
 
 using namespace casa;
+
+IPosition handlePos (const IPosition& pos, const IPosition& def)
+{
+  if (pos.nelements() == 0) {
+    return def;
+  }
+  if (pos.nelements() != 2) {
+    throw AipsError("Give 0 or 2 values in maskblc and masktrc");
+  }
+  IPosition npos(def);
+  int n = npos.nelements();
+  npos[n-1] = pos[1];
+  npos[n-2]= pos[0];
+  return npos;
+}
+
+IPosition readIPosition (const String& in)
+{
+  if (in.empty()) {
+    return IPosition();
+  }
+  Vector<String> strs = stringToVector (in);
+  IPosition ipos(strs.nelements());
+  for (uInt i=0; i<strs.nelements(); ++i) {
+    istringstream iss(strs[i]);
+    iss >> ipos[i];
+  }
+  return ipos;
+}
 
 Quantity readQuantity (const String& in)
 {
@@ -77,22 +108,31 @@ int main (Int argc, char** argv)
   try {
     Input inputs(1);
     // define the input structure
-    inputs.version("20080516-GvD");
+    inputs.version("20080818-GvD");
     inputs.create ("ms", "",
 		   "Name of input MeasurementSet",
 		   "string");
     inputs.create ("image", "",
 		   "Name of output image file (default is <msname-stokes-mode-nchan>.img)",
 		   "string");
-    inputs.create ("fits", "",
-		   "Name of output fits file ('no' means no fits file) default is <imagename>.fits",
+    inputs.create ("fits", "no",
+		   "Name of output image fits file ('no' means no fits file) empty is <imagename>.fits",
+		   "string");
+    inputs.create ("hdf5", "no",
+    		   "Name of output image HDF5 file ('no' means no HDF5 file) empty is <imagename>.hdf5",
+    		   "string");
+    inputs.create ("model", "",
+		   "Name of model image file (default is <imagename>.model",
+		   "string");
+    inputs.create ("restored", "",
+		   "Name of restored image file (default is <imagename>.restored",
+		   "string");
+    inputs.create ("residual", "",
+		   "Name of residual image file (default is <imagename>.residual",
 		   "string");
     inputs.create ("data", "DATA",
 		   "Name of DATA column to use",
 		   "string");
-    inputs.create ("hdf5", "false",
-    		   "Create the image in HDF5 format?"
-    		   "bool");
     inputs.create ("mode", "mfs",
 		   "Imaging mode (mfs, channel, or velocity)",
 		   "string");
@@ -116,10 +156,10 @@ int main (Int argc, char** argv)
 		   "int");
     inputs.create ("cellsize", "1arcsec",
 		   "pixel width in x and y direction",
-		   "string");
+		   "quantity string");
     inputs.create ("phasecenter", "",
 		   "phase center to be used (e.g. 'j2000, 05h30m, -30.2deg')",
-		   "string");
+		   "direction string");
     inputs.create ("field", "0",
 		   "field id to be used",
 		   "int");
@@ -150,12 +190,39 @@ int main (Int argc, char** argv)
     inputs.create ("select", "",
 		   "TaQL selection string for MS",
 		   "string");
+    inputs.create ("operation", "image",
+		   "Operation (image,clark,hogbom)",
+		   "string");
+    inputs.create ("niter", "1000",
+		   "Number of clean iterations",
+		   "int");
+    inputs.create ("gain", "0.1",
+		   "Loop gain for cleaning",
+		   "float");
+    inputs.create ("threshold", "0Jy",
+		   "Flux level at which to stop cleaning",
+		   "quantity string");
+    inputs.create ("fixed", "False",
+		   "Keep clean model fixed",
+		   "bool");
+    inputs.create ("mask", "",
+		   "Name of the mask to use in cleaning",
+		   "string");
+    inputs.create ("maskblc", "0,0",
+		   "bottom-left corner of mask region",
+		   "int vector");
+    inputs.create ("masktrc", "image shape",
+		   "top-right corner of mask region",
+		   "int vector");
+    inputs.create ("maskvalue", "-1.0",
+		   "Value to store in mask region; if given, mask is created; if mask not exists, defaults to 1.0",
+		   "float");
 
     // Fill the input structure from the command line.
     inputs.readArguments (argc, argv);
 
     // Get the input specification.
-    Bool hdf5        = inputs.getBool("hdf5");
+    Bool fixed       = inputs.getBool("fixed");
     Long cachesize   = inputs.getInt("cachesize");
     Int fieldid      = inputs.getInt("field");
     Int spwid        = inputs.getInt("spwid");
@@ -167,18 +234,30 @@ int main (Int argc, char** argv)
     Int img_start    = inputs.getInt("img_chanstart");
     Int img_step     = inputs.getInt("img_chanstep");
     Int wplanes      = inputs.getInt("wprojplanes");
+    Int niter        = inputs.getInt("niter");
     Double padding   = inputs.getDouble("padding");
+    Double gain      = inputs.getDouble("gain");
+    Double maskValue = inputs.getDouble("maskvalue");
     String mode      = inputs.getString("mode");
+    String operation = inputs.getString("operation");
     String weight    = inputs.getString("weight");
     String stokes    = inputs.getString("stokes");
     String chanmode  = inputs.getString("chanmode");
     String cellsize  = inputs.getString("cellsize");
     String phasectr  = inputs.getString("phasecenter");
+    String threshStr = inputs.getString("threshold");
     String msName    = inputs.getString("ms");
     String imgName   = inputs.getString("image");
     String fitsName  = inputs.getString("fits");
+    String hdf5Name  = inputs.getString("hdf5");
+    String modelName = inputs.getString("model");
+    String restoName = inputs.getString("restored");
+    String residName = inputs.getString("residual");
     String imageType = inputs.getString("data");
     String select    = inputs.getString("select");
+    String maskName  = inputs.getString("mask");
+    String mstrBlc   = inputs.getString("maskblc");
+    String mstrTrc   = inputs.getString("masktrc");
 
     // Check and interpret input values.
     Quantity qcellsize = readQuantity (cellsize);
@@ -207,20 +286,31 @@ int main (Int argc, char** argv)
       imgName += '-' + stokes + '-' + mode + String::toString(img_nchan)
 	+ ".img";
     }
-    if (fitsName.empty()) {
-      fitsName = imgName;
-      fitsName.gsub (Regex("\\..*"), ".fits");
+    if (fitsName == "no") {
+      fitsName = String();
+    } else if (fitsName.empty()) {
+      fitsName = imgName + ".fits";
     }
-    String hdf5Name;
-    if (hdf5) {
-#ifndef HAVE_HDF5
+    if (hdf5Name == "no") {
+      hdf5Name = String();
+#ifdef HAVE_HDF5
+    } else if hdf5Name.empty()) {
+      hdf5Name = imgName + ".hdf5";
+#else
+    } else {
       std::cerr << "Cannot create image in HDF5 format"
 		<< " (HDF5 support is not compiled in)" << std::endl;
-      hdf5 = False;
-#else
-      hdf5Name = imgName;
-      imgName += "_tmp";
+      hdf5Name = String();
 #endif
+    }
+    if (modelName.empty()) {
+      modelName = imgName + ".model";
+    }
+    if (restoName.empty()) {
+      restoName = imgName + ".restored";
+    }
+    if (residName.empty()) {
+      residName = imgName + ".residual";
     }
     if (weight == "robust") {
       weight = "briggs";
@@ -231,10 +321,17 @@ int main (Int argc, char** argv)
       doShift = True;
       phaseCenter = readDirection (phasectr);
     }
+    operation.downcase();
+    AlwaysAssertExit (operation=="image" || operation=="hogbom" || operation=="clark");
+    IPosition maskBlc, maskTrc;
+    Quantity  threshold;
+    if (operation != "image") {
+      maskBlc = readIPosition (mstrBlc);
+      maskTrc = readIPosition (mstrTrc);
+      threshold = readQuantity (threshStr);
+    }
 
-    std::cerr << phaseCenter<< std::endl;
-
-    // Do the imaging.
+    // Set the various imager variables.
     // The non-parameterized values used are the defaults in imager.g.
     MeasurementSet ms(msName, Table::Update);
     Imager imager(ms);
@@ -292,32 +389,69 @@ int main (Int argc, char** argv)
 		      True,                         // applyPointingOffsets
 		      True,                         // doPointingCorrection
 		      "");                          // cfCacheDirName
-    imager.makeimage (imageType, imgName, "");
 
-    // Convert to HDF5 if needed.
+    // Do the imaging.
+    if (operation == "image") {
+      imager.makeimage (imageType, imgName, "");
+
+      // Convert to HDF5 if needed.
 #ifdef HAVE_HDF5
-    if (hdf5) {
-      PagedImage<float> pimg(imgName);
-      HDF5Image<float>  himg(pimg.shape(), pimg.coordinates(), hdf5Name);
-      himg.copyData (pimg);
-      himg.setUnits     (pimg.units());
-      himg.setImageInfo (pimg.imageInfo());
-      himg.setMiscInfo  (pimg.miscInfo());
-    }
-#endif
-
-    // Convert to fits if needed.
-    if (fitsName != "no") {
-      String error;
-      PagedImage<float> img(imgName);
-      if (! ImageFITSConverter::ImageToFITS (error, img, fitsName)) {
-	throw AipsError(error);
+      if (! hdf5Name.empty()) {
+	PagedImage<float> pimg(imgName);
+	HDF5Image<float>  himg(pimg.shape(), pimg.coordinates(), hdf5Name);
+	himg.copyData (pimg);
+	himg.setUnits     (pimg.units());
+	himg.setImageInfo (pimg.imageInfo());
+	himg.setMiscInfo  (pimg.miscInfo());
       }
-    }
+#endif
+      // Convert result to fits if needed.
+      if (! fitsName.empty()) {
+	String error;
+	PagedImage<float> img(imgName);
+	if (! ImageFITSConverter::ImageToFITS (error, img, fitsName)) {
+	  throw AipsError(error);
+	}
+      }
 
-    // Delete PagedImage if HDF5 was used.
-    if (hdf5) {
-      Table::deleteTable (imgName);
+//      // Delete PagedImage if HDF5 was used.
+//      if (! hdf5Name.empty()) {
+//        Table::deleteTable (imgName);
+//      }
+
+    } else {
+    // Do the cleaning.
+      if (! maskName.empty()) {
+	if (maskValue >= 0) {
+	  PagedImage<float> pimg(imgName);
+	  maskBlc = handlePos (maskBlc, IPosition(pimg.ndim(), 0));
+	  maskTrc = handlePos (maskTrc, pimg.shape() - 1);
+	  imager.boxmask (maskName,
+			  maskBlc.asVector(),
+			  maskTrc.asVector(),
+			  maskValue);
+	}
+      }
+      imager.clean (operation,                      // algorithm,
+		    niter,                          // niter
+		    gain,                           // gain
+		    threshold,                      // threshold
+		    False,                          // displayProgress
+		    Vector<String>(1, modelName),   // model
+		    Vector<Bool>(1, fixed),         // fixed
+		    "",                             // complist
+		    Vector<String>(1, maskName),    // mask
+		    Vector<String>(1, restoName),   // restored
+		    Vector<String>(1, residName));  // residual
+      // Convert result to fits if needed.
+       
+      if (! fitsName.empty()) {
+	String error;
+	PagedImage<float> img(restoName);
+	if (! ImageFITSConverter::ImageToFITS (error, img, fitsName)) {
+	  throw AipsError(error);
+	}
+      }
     }
 
   } catch (AipsError x) {
